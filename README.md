2ebccbc7fd7cf71b3d33e1119f1888127e7c5358
dharamraj-dev <dharamrajyadav6312@gmail.com>
2024-01-30 12:43:18 +0530

    Complete To DO List App

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# react-to-do-list
+# React-Landing-Page-Upflairs
+
+This project demonstrates a basic To-Do List using the React (React, Hooks, Map, Filter) stack.
+
+## Overview
+
+- **React.js**: Front-end framework for building user interfaces.
+
+## Screenshots
+
+![Main Page](screenshot/ScreenSHot.png)
+*Screenshot of the Main page.*
+
+## Features
+
+- Tailwind Use in this project.
+- Responsive design for a better user experience.
+
+## Getting Started
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/Dharam-IN/react-to-do-list
+    ```
+
+2. Install dependencies:
+
+    ```bash
+    cd your-repo
+    npm install
+    ```
+
+3. Set up the environment variables:
+
+    Create a `.env` file in the root directory and add the following:
+
+    ```env
+    REACT_APP_API=http://localhost:your-api-port
+    ```
+
+4. Run the application:
+
+    ```bash
+    npm start
+    ```
+
+    The app will be available at `http://localhost:5173` by default.
+
+## Contributing
+
+Feel free to contribute by opening issues or pull requests. Feedback and suggestions are welcome!
+
+## License
+
+<!-- This project is licensed under the [MIT License](LICENSE). -->
+
--- react-to-do/src/components/Form.jsx
+++ react-to-do/src/components/Form.jsx
@@ -1,17 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { MdDelete } from "react-icons/md";
 
 const Form = () => {
+    const [value, setValue] = useState('');
+    const [listData, setListdata] = useState([]);
 
-    const [value, setValue] = useState('')
+    const handleSubmit = () => {
+        setListdata([...listData, value]);
+        setValue('');
+    }
 
-  return (
-    <div>
-        <form className='mb-4 w-full font-primary text-center'>
-            <input type="text" onChange={(e)=>console.log(e.target.value)} className='outline-none border border-gray-500 rounded-xl text-white me-3 p-4 bg-transparent w-[300px] placeholder:text-gray-300' placeholder='What task have you today?'/>
-            <button type='button' className='bg-gray-700 border-none p-2 text-white cursor-pointer rounded-md'>Add Task</button>
-        </form>
-    </div>
-  )
+    const removeItem = (index) => {
+        const updatedList = listData.filter((item, i) => i !== index);
+        setListdata(updatedList);
+    }
+
+    return (
+        <div className='mx-auto w-[350px]'>
+            <div className='flex'>
+              <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className='outline-none border border-gray-500 rounded-xl text-white me-3 p-4 bg-transparent w-[70%] placeholder:text-gray-300' placeholder='What task have you today?' />
+              <button className='w-[30%] bg-gray-700 border-none p-2 text-white cursor-pointer rounded-md' onClick={handleSubmit}>Add Task</button>
+            </div>
+            <p className='text-white text-start text-xl mt-5 mb-3'>Here is your list</p>
+            {listData.length > 0 && listData.map((data, i) => (
+                <div key={i} className='text-white bg-lime-600 p-4 mb-2 flex justify-between items-center'>
+                    <p className='capitalize font-semibold text-3xl'>{data}</p>
+                    <i className='text-xl cursor-pointer hover:text-orange-600 text-3xl' onClick={() => removeItem(i)}>
+                        <MdDelete />
+                    </i>
+                </div>
+            ))}
+        </div>
+    );
 }
 
-export default Form
+export default Form;
